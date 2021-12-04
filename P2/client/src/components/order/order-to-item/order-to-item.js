1436b25c60c3cac0cf5fc41f40bd2ed62d940d9a
Haoting Qiu <haotingqiu@gmail.com>
2021-12-04 18:23:23 -0500

    add order, order-to-item logics to client, fix bug

--- P2/client/src/components/order/order-to-item/order-to-item.js
+++ P2/client/src/components/order/order-to-item/order-to-item.js
@@ -2,11 +2,11 @@ import OrderToItemService from "../../../services/order-to-item-service";
 import React, {useState, useEffect} from "react";
 
 const OrderToItem = (props) => {
-    const {id, menuItems} = props;
+    const {orderToMenuItemId, menuItems, findOrderToItems, orderId} = props;
     const [orderToItem, setItem] = useState({});
-
+    const [editting, setEditting] = useState(false);
     useEffect(() => {
-        findOrderToItemById(id);
+        findOrderToItemById(orderToMenuItemId);
     }, [])
 
     const findOrderToItemById = async (id) => {
@@ -18,9 +18,10 @@ const OrderToItem = (props) => {
         }
     };
 
-    const deleteOrderToItem = async (id) => {
+    const deleteOrderToItem = async (id, orderId) => {
         try {
             await OrderToItemService.deleteOrderToItem(id);
+            findOrderToItems(orderId);
         } catch (e) {
             console.log(e);
         }
@@ -31,6 +32,7 @@ const OrderToItem = (props) => {
         try {
             const response = await OrderToItemService.updateOrderToItem(id, orderToItem);
             setItem(response);
+            setEditting(false);
         } catch (e) {
             console.log(e);
         }
@@ -42,7 +44,11 @@ const OrderToItem = (props) => {
         <select value={orderToItem.menuItemId} onChange={(e) => setItem({...orderToItem, menuItemId: e.target.value})}>
             <option></option>
             {
-                menuItems.map((m, idx) =><option key={idx} value={m.menuItemId}>{m.name} ${m.price}</option>)
+                menuItems.map((m, idx) =>{
+                    if (m.menuItemId === orderToItem.menuItemId) 
+                        return <option key={idx} value={m.menuItemId} defaultValue>{m.name} ${m.price}</option>
+                    return <option key={idx} value={m.menuItemId}>{m.name} ${m.price}</option>
+                })
             }
         </select>
             <br/>           
@@ -51,14 +57,30 @@ const OrderToItem = (props) => {
                    value={orderToItem.quantity}
                    onChange={(e) => setItem({...orderToItem, quantity: e.target.value})}
             />
-            <button className="btn btn-danger me-3"
-                    onClick={() => deleteOrderToItem(orderToItem.orderToMenuItemId)}>
-                Delete
-            </button>  
-            <button onClick={() => updateOrderToItem(orderToItem.orderToMenuItemId, orderToItem)}
+            {
+                editting && <button className="btn btn-danger me-3"
+                onClick={() => deleteOrderToItem(orderToItem.orderToMenuItemId, orderId)}>
+                    Delete
+                </button>  
+            }
+            {
+                editting && <button onClick={() => updateOrderToItem(orderToItem.orderToMenuItemId, orderToItem)}
                     className="btn btn-primary me-3">
-                Save
-            </button>                               
+                    Save
+                </button> 
+            }
+            {
+                editting && <button onClick={() => setEditting(false)}
+                    className="btn btn-primary me-3">
+                    Cancel
+                </button>
+            }
+            {
+                !editting && <button onClick={() => setEditting(true)}
+                className="btn btn-primary me-3">
+                    Edit
+                </button>
+            }                            
             <br/>
         </div>
     )
